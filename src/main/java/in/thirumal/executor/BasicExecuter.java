abdeffb7aa1d0cee183d317bbd5876c173204bbf
Thirumal <m.thirumal@hotmail.com>
2020-01-11 21:50:40 +0530

    Updated

---
+++ src/main/java/in/thirumal/executor/BasicExecuter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package in.thirumal.executor;
+
+/**
+ * @author thirumal
+ *
+ */
+public class BasicExecuter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/in/thirumal/thread/BasciThread.java
+++ src/main/java/in/thirumal/thread/BasciThread.java
@@ -16,18 +16,20 @@ public class BasciThread extends Thread{
 		Thread t = new BasciThread();
 		System.out.println(t.getClass());
 		t.start();
+		// t.start(); //Not allowed
+		///t.run(); //Runs on same thread
 		System.out.println("hello");
 	}
 	
 	
 	public void run() {
 		try {
-			Thread.sleep(12222);
+			Thread.sleep(122);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("ji");
+		System.out.println(Thread.currentThread().getName());
 	}
 
 }
