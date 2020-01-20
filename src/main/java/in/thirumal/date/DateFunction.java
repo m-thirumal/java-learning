f638cd2051eadf47847fc980ed49f462f500ab26
Thirumal <m.thirumal@hotmail.com>
2020-01-20 18:20:05 +0530

    Added date function

---
+++ src/main/java/in/thirumal/date/DateFunction.java
@@ -0,0 +1,11 @@
+package in.thirumal.date;
+
+import java.util.Date;
+
+public class DateFunction {
+
+	public static void main(String[] args) {
+		System.out.println(new Date().after(new Date()));
+	}
+
+}
