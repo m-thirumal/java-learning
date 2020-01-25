20e431f1eaaf13e240ba1faa77bf56c1255d6173
Thirumal <m.thirumal@hotmail.com>
2020-01-25 16:43:42 +0530

    long

---
+++ src/main/java/in/thirumal/datatype/LongLearning.java
@@ -0,0 +1,12 @@
+package in.thirumal.datatype;
+
+public class LongLearning {
+
+	public static void main(String[] args) {
+		Long a = Long.MAX_VALUE;
+		System.out.println(a == Long.MAX_VALUE);
+		System.out.println(a.compareTo(Long.MAX_VALUE) == 0);
+		System.out.println(a.equals(Long.MAX_VALUE));
+	}
+
+}
