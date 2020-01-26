492fc853075257d9517869a19670a96a9a6c09a1
Thirumal <m.thirumal@hotmail.com>
2020-01-26 12:36:18 +0530

    e

--- src/main/java/in/thirumal/date/DateFunction.java
+++ src/main/java/in/thirumal/date/DateFunction.java
@@ -1,11 +1,18 @@
 package in.thirumal.date;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class DateFunction {
 
 	public static void main(String[] args) {
-		System.out.println(new Date().after(new Date()));
+		Date date1 = new Date("01/26/2020");
+		Date date2 = new Date();;
+		System.out.println(date1);
+		System.out.println(date2);
+		System.out.println(date1.equals(date2));
+		System.out.println(date1.before(date2));
 	}
 
 }
